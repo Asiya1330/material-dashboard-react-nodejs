37f3cc332506ed29545bcc604da324b8f8348c10
Bogdan Mihalcea <bogdan.mihalcea@icloud.com>
2024-03-27 10:16:52 +0200

    fixed password reset vulnerability

--- node-api/src/routes/index.js
+++ node-api/src/routes/index.js
@@ -1,5 +1,8 @@
 import userRoutes from './users';
 import meRoutes from './me';
 import authRoutes from './auth';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export { userRoutes, meRoutes, authRoutes };
